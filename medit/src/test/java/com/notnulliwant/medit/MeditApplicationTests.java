e8cab4d8498ab9c585513db836e06e37dc45fbe7
explorersm11 <explorersm@naver.com>
2024-02-12 00:39:16 +0900

    프로젝트 기반 잡기

---
+++ medit/src/test/java/com/notnulliwant/medit/MeditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notnulliwant.medit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
