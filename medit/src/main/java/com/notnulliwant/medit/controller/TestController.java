7c55d05462ff75d58b4690a258eef7d55fb4cd17
explorersm11 <explorersm@naver.com>
2024-02-12 00:19:01 +0900

    test

---
+++ medit/src/main/java/com/notnulliwant/medit/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.notnulliwant.medit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/test")
+	public String test() {
+		return "Test";
+	}
+	
+}
