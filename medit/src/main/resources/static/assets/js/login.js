805910596ee31114c2fea630b8a4199377889caa
explorersm11 <explorersm@naver.com>
2024-02-15 02:20:55 +0900

    윤수민 : 로그인 화면 수정

--- medit/src/main/resources/static/assets/js/login.js
+++ medit/src/main/resources/static/assets/js/login.js
@@ -20,3 +20,46 @@ pw.addEventListener("click", function(e) {
   
   pw.classList.add("bounce");
 }, false);
+
+
+let body = document.querySelector('body');
+let fingerprint = document.querySelector('.fingerprint');
+let center = document.querySelector('.center');
+let anno = document.querySelector('#anno');
+let timer, timerSuccess;
+let submit = document.querySelector("#submit");
+
+function onSuccess() {
+  body.removeEventListener('mousedown', onTouchStart);
+  body.removeEventListener('touchstart', onTouchStart);
+
+  fingerprint.classList.remove('active');
+  center.classList.add('success');
+
+  anno.className += "smooth";
+
+  clearTimeout(timerSuccess);
+
+  timerSuccess = setTimeout(() => {
+    body.addEventListener('mousedown', onTouchStart);
+    body.addEventListener('touchstart', onTouchStart);
+
+    submit.submit();
+    center.classList.remove('success');
+  }, 3000);
+}
+
+function onTouchStart() {
+  fingerprint.classList.add('active');
+  timer = setTimeout(onSuccess, 2000);
+}
+
+function onTouchEnd() {
+  fingerprint.classList.remove('active');
+  clearTimeout(timer);
+}
+
+body.addEventListener('mousedown', onTouchStart);
+body.addEventListener('touchstart', onTouchStart);
+body.addEventListener('mouseup', onTouchEnd);
+body.addEventListener('touchend', onTouchEnd);
