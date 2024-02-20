71d90f2aeb08c3cc579494c48f230ebb61a47d63
smhrd <smhrd@192.168.219.55>
2024-02-20 13:59:19 +0900

    진단날짜출력

--- medit/src/main/resources/static/assets/js/AtPatient.js
+++ medit/src/main/resources/static/assets/js/AtPatient.js
@@ -1,4 +1,4 @@
-let diagAt = $("#diagnosisDate");
+let diagDetail = $("#diagnosisDate");
 
 $(document).on('click', '.selectPtnt', function() {
 
@@ -8,32 +8,28 @@ $(document).on('click', '.selectPtnt', function() {
 		data: { "ptntId": seletedPtntId },
 		success: function(res) {
 
-			let Arr_ptntAt = []; // 
+			let Arr_ptntAt = [];
 
 			for (let i = 0; i < res.length; i++) {
-
+				
 				let originalDate = res[i].diagAt
 				let formattedDate = originalDate.substring(0, 16).replace('T', ' ');
 				formattedDate = formattedDate.replace(/:/, '시 ') + '분';
 				Arr_ptntAt.push(formattedDate);
-
+				
 			}
 
 			// 테이블 생성
-			/*let tableHtml = '<table>';*/
+			let tableHtml = '<table>';
 
-			/*for (let i = 0; i < res.length; i++) {*/
-				diagAt.html( `
-				for (let i = 0; i < res.length; i++) {
-					<table>
-	                    <tr>
-	                        <td>${Arr_ptntAt[i]}</td>
-	                    </tr> 
-					</table>}
-					`).trigger("create");
-			/*}*/
+			for (let i = 0; i < res.length; i++) {
+				tableHtml += `
+                    <tr>
+                        <td>${Arr_ptntAt[i]}</td>
+                    </tr>`;
+			}
 
-		/*	tableHtml += '</table>';*/
+			tableHtml += '</table>';
 
 			diagDetail.html(tableHtml).trigger("create");
 		}
