bfe04052adb01b56caaa957fbf0643c445d2e82d
smhrd <smhrd@192.168.219.46>
2024-02-20 12:28:17 +0900

    채팅 기능 수정

--- medit/src/main/java/com/notnulliwant/medit/controller/YH_Controller.java
+++ medit/src/main/java/com/notnulliwant/medit/controller/YH_Controller.java
@@ -8,10 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.notnulliwant.medit.entity.Chattings;
 import com.notnulliwant.medit.entity.Doctors;
 import com.notnulliwant.medit.repository.ChatroomMembersRepository;
+import com.notnulliwant.medit.repository.ChattingsRepository;
 import com.notnulliwant.medit.repository.DoctorsRepository;
 
 @Controller
@@ -22,6 +27,9 @@ public class YH_Controller {
 	
 	@Autowired
 	private ChatroomMembersRepository crm_repo;
+	
+	@Autowired
+	private ChattingsRepository c_repo;
 	
 	@RequestMapping("/dlist")
 	public String dlist(HttpSession session, Model model) {
@@ -34,14 +42,18 @@ public class YH_Controller {
 	
 	@RequestMapping("/doctor/{id}")
 	public String doctorDetail(@PathVariable("id") String doctorId, HttpSession session, Model model) {
-		Doctors d_s = (Doctors) session.getAttribute("user");
-	    Doctors doc = d_repo.findByDoctorId(doctorId);
-	    Integer roomSeq = crm_repo.findRoomSeqByDoctorIds(d_s.getDoctorId(), doctorId);
+		Doctors doctor = (Doctors) session.getAttribute("user");
+	    Integer roomSeq = crm_repo.findRoomSeqByDoctorIds(doctor.getDoctorId(), doctorId);
 	    
 	    model.addAttribute("roomseq", roomSeq);
 	    
 	    return "yh";
 	}
 
-
+//	@PostMapping("/chat/history")
+//	@ResponseBody
+//	public List<Chattings> getChatHistory(@RequestParam Integer roomSeq) {
+//	    return c_repo.findByRoomSeq(roomSeq);
+//	}
+	
 }
