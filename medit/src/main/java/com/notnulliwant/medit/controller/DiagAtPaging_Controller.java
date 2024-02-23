93be7058b087670cbcb1acb5f514d7f460246baa
smhrd <smhrd@192.168.219.55>
2024-02-23 09:15:10 +0900

    병훈커밋 진단날짜 의사소견

---
+++ medit/src/main/java/com/notnulliwant/medit/controller/DiagAtPaging_Controller.java
@@ -0,0 +1,54 @@
+package com.notnulliwant.medit.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.notnulliwant.medit.entity.PagingPatients;
+import com.notnulliwant.medit.entity.Patients;
+import com.notnulliwant.medit.repository.DiagnosisRepository;
+import com.notnulliwant.medit.repository.PatientsRepository;
+import com.notnulliwant.medit.service.DiagnosisService;
+import com.notnulliwant.medit.service.PatientsService;
+
+@RestController
+public class DiagAtPaging_Controller {
+
+	@Autowired
+	private DiagnosisService diagnosisService;
+
+	@Autowired
+	private DiagnosisRepository diagnosisRepo;
+
+	// 환자 목록 페이징 //
+	/*
+	 * @GetMapping("/paging") public PagingPatients paging(@PageableDefault(page =
+	 * 1) Pageable pageable, Model mdl) { Page<Patients> ptntList =
+	 * diagnosisService.paging(pageable);
+	 * 
+	 * List<Patients> ptnts = ptntList.getContent();
+	 * 
+	 * PagingPatients pagingpatients = new PagingPatients();
+	 * 
+	 * int blockLimit = 5; // 페이지 개수 조정 int startPage = (((int) Math.ceil(((double)
+	 * pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; int endPage =
+	 * Math.min((startPage + blockLimit - 1), ptntList.getTotalPages());
+	 * 
+	 * pagingpatients.setPatients(ptnts);
+	 * pagingpatients.setFirst(ptntList.isFirst());
+	 * pagingpatients.setLast(ptntList.isLast());
+	 * pagingpatients.setNumber(ptntList.getNumber());
+	 * pagingpatients.setTotalPage(ptntList.getTotalPages());
+	 * pagingpatients.setStartPage(startPage); pagingpatients.setEndPage(endPage);
+	 * 
+	 * return pagingpatients; }
+	 */
+}
--- medit/src/main/java/com/notnulliwant/medit/repository/DiagnosisRepository.java
+++ medit/src/main/java/com/notnulliwant/medit/repository/DiagnosisRepository.java
@@ -2,7 +2,10 @@ package com.notnulliwant.medit.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,5 +18,14 @@ public interface DiagnosisRepository extends JpaRepository<Diagnosis, Integer> {
    //@Query(value="SELECT * FROM DIAGNOSIS WHERE PTNT_ID = :ptntId", nativeQuery=true)
    List<Diagnosis> findAllByPtntId(Integer ptntId); // BH , 환자 번호가져올거임
    
+   Diagnosis findAllBydiagSeq(Integer diagSeq);  // 의사소견쪽
    
+   
+	/* void updateDoctorOpinionByDiagSeq(Integer diagSeq, String doctorOpinion); */
+  @Transactional
+  @Modifying
+  @Query("UPDATE Diagnosis d SET d.doctorOpinion = :doctorOpinion WHERE d.diagSeq = :diagSeq")
+  void updateDoctorOpinionByDiagSeq(@Param("diagSeq") Integer diagSeq, @Param("doctorOpinion") String doctorOpinion);
+  
+  
 }
