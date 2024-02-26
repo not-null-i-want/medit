f596c63e8b571e0c53870507b19e8b3698f6ac56
smhrd <smhrd@192.168.219.55>
2024-02-26 10:04:16 +0900

    controller 수정

--- medit/src/main/java/com/notnulliwant/medit/controller/RestDiagnosis_Controller.java
+++ medit/src/main/java/com/notnulliwant/medit/controller/RestDiagnosis_Controller.java
@@ -1,29 +1,31 @@
 package com.notnulliwant.medit.controller;
 
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.notnulliwant.medit.entity.Diagnosis;
-import com.notnulliwant.medit.entity.Patients;
 import com.notnulliwant.medit.repository.DiagnosisRepository;
-import com.notnulliwant.medit.repository.PatientsRepository;
 
 @Controller
-public class Opinion_Controller {
+public class RestDiagnosis_Controller {
 
 	@Autowired
 	private DiagnosisRepository Diagrepo;
 
+	@ResponseBody
+	@RequestMapping("/ShowPatientAt") // 진달 날짜 띄워주는 컨트롤러임 // 트러블슈팅, postmapping으로해놔서 오류가 405오류가 뜨던걸 requestmapping으로	바꿔주니 해결됨!
+	public List<Diagnosis> ShowPatientAt(Integer ptntId) {
+
+		List<Diagnosis> diag = Diagrepo.findAllByPtntId(ptntId);
+
+	return diag;
+	
+		}
+	
 	@ResponseBody
 	@RequestMapping("/ShowDiagOpinion") // 의사소견 띄워주는 컨트롤러
 	public Diagnosis showDiagOpinion(Integer diagSeq) {
@@ -35,7 +37,7 @@ public class Opinion_Controller {
 	}
 
 	@ResponseBody
-	@RequestMapping("/saveOpinion") // 의사소견 저장해주는 컨트롤러
+	@RequestMapping("/saveOpinion")
 	public String updateOpinion(Integer saveSeq, String saveOpinion) {
 
 		System.out.println(saveOpinion);
@@ -49,5 +51,5 @@ public class Opinion_Controller {
 
 		return "";
 	}
-
+	 	
 }
